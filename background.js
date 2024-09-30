d2961a61f40aad2045deb4f9085b367b2ce852fe
L413 <53121093+L413@users.noreply.github.com>
2024-09-29 23:25:14 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,28 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url !== "https://shelfconsole.glitch.me/") {
-        chrome.tabs.update(tabId, { url: "https://shelfconsole.glitch.me/" });
+let isKioskMode = false;
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === "toggle-kiosk") {
+        isKioskMode = !isKioskMode;
+        if (isKioskMode) {
+            openKioskTab();
+        }
     }
 });
+
+function openKioskTab() {
+    chrome.tabs.create({ url: "https://shelfconsole.glitch.me/" }, (tab) => {
+        chrome.tabs.onUpdated.addListener(function checkTab(tabId, changeInfo) {
+            if (tabId === tab.id && changeInfo.status === 'complete') {
+                chrome.tabs.executeScript(tab.id, {
+                    code: `
+                        document.documentElement.requestFullscreen();
+                        window.onbeforeunload = function() {
+                            return "Are you sure you want to exit Kiosk Mode?";
+                        };
+                    `
+                });
+                chrome.tabs.onUpdated.removeListener(checkTab);
+            }
+        });
+    });
+}
